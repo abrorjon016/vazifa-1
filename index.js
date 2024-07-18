c0ebb1638a1f1f2d4e9a3769979ee9612ca73a17
abrorjon016 <ikromjonovabrorjon3@gmail.com>
2024-07-18 21:28:58 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,116 @@
+// 1-masala
+
+// let num = 700;
+
+// let res = num / 100;
+
+// console.log(res);
+
+//
+
+//
+
+// 2-masala
+// let num = 7000;
+
+// let res = num / 1000;
+
+// console.log(res);
+
+//
+
+//
+
+// 3-masala
+// let num = 7000;
+//
+
+// let res = num1 / 1024;
+
+// console.log(res);
+
+//
+
+//
+
+// 4-masala
+// let num1 = 7000;
+// let num2 = 1000;
+
+// let res = num1 / num2;
+
+// console.log(res);
+
+//
+
+//
+
+// 5-masala
+// let num1 = 10;
+// let num2 = 3;
+
+// let res = num1 / num2;
+// let res2 = num1 % num2;
+
+// console.log(res2);
+// console.log(Math.floor(res));
+
+// 6-masala
+// let num = 32;
+// let res = Math.trunc(num / 10) % 10;
+// let res2 = num % 10;
+
+// console.log(res);
+// console.log(res2);
+
+// 7-masala
+// let num = 22;
+// let res = Math.trunc(num / 10) % 10;
+// let res2 = num % 10;
+
+// let res3 = res + res2;
+// console.log(res3);
+
+// 8-masala
+// let num = 12;
+// let res = Math.trunc(num / 10) % 10;
+// let res2 = num % 10;
+
+// console.log(res2, res);
+
+// 9-masala
+// let num = 32;
+// let res = Math.trunc(num / 100) % 10;
+
+// console.log(res);
+
+// 10-masala
+// let num = 32;
+// let res = Math.trunc(num / 10) % 10;
+// let res2 = num % 10;
+
+// console.log(res);
+// console.log(res2);
+
+// 11-masala
+// let num = 222;
+// let res = Math.trunc(num / 100) % 100;
+// let res2 = num % 100;
+
+// let res3 = res + res2;
+// console.log(res3);
+
+// 12 - masala;
+// let num = 123;
+// let res = Math.trunc(num / 100) % 100;
+// let res2 = Math.trunc(num / 10) % 10;
+// let res3 = num % 10;
+
+// console.log(res3, res2, res);
+
+// 13 - masala;
+// let num = 123;
+// let res = Math.trunc(num / 100) % 100;
+// let res2 = Math.trunc(num / 10) % 10;
+// let res3 = num % 10;
+// console.log( res2, res);
